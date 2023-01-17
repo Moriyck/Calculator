5f0e76b44215e0a15d63b54decc7b70c237cc77f
Moriyck <Sim19720504@yandex.ru>
2023-01-17 15:43:47 +0300

    З

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,27 @@ public class Main {
     public static void main(String[] args) {
 
         char firstCharacter;
-        char secondCharacter;
-        char thirdCharacter;
-        char fourthCharacter;
-        char fifthCharacter;
+
         int firstNumber;
-        char operator;
+        int secondNumber;
+
+        String operator;
         int operatorIndex;
 
-        int secondNumber;
         int result;
         Boolean operatorSelection;
-        Input_method inputMethod;
-        int theNumberTen;
 
         Input_errors inputErrors;
         String inputErrorsString;
 
         String[] operatorsStrings = { "+", "-", "/", "*" };
+        String[] regexActions = { "\\+", "-", "/", "\\*" };
+
         operatorIndex = -1;
 
         Scanner keyboardScanner = new Scanner(System.in);
         System.out.println("Input: ");
         String inputString = keyboardScanner.nextLine();
-
-        int[] inputStringArry = new int[inputString.length()];
-
-        for (int i = 0; i < inputStringArry.length; i++) {
-            inputStringArry[i] = inputString.toCharArray()[i];
-        }
 
         for (int i = 0; i < operatorsStrings.length; i++) {
             if (inputString.contains(operatorsStrings[i])) {
@@ -50,38 +42,97 @@ public class Main {
         if (operatorIndex == -1) {
             inputErrors = Input_errors.STRING_NOT_MATH_OPERATION;
             inputErrorsString = inputErrors.getError();
+            System.out.println(inputErrorsString);
             return;
         }
 
-        System.out.println(operatorIndex);
+        String[] data = inputString.split(regexActions[operatorIndex]);
+        operator = operatorsStrings[operatorIndex];
 
-        firstCharacter = inputString.charAt(0);
+        String numberOneString = data[0];
+        for (int i = 0; i < numberOneString.length(); i++) {
+            String letter = Character.toString(numberOneString.charAt(i));
+            // int pos = numberOneString.indexOf(i);
 
-        firstNumber = Character.digit(inputString.charAt(0), 10);
-        operator = inputString.charAt(1);
-        secondNumber = Character.digit(inputString.charAt(2), 10);
+            Roman_numerals rNumerals = Roman_numerals.valueOf(letter); // II =2
+
+            System.out.println(rNumerals.displayRomNum);
+            System.out.println(letter);
+            System.out.println(rNumerals);
+        }
+
+        // firstNumber = Integer.parseInt(data[0]);
+        // secondNumber = Integer.parseInt(data[1]);
+
+        firstNumber = 1;
+        secondNumber = 1;
+
+        Input_method inputMethodNum = Input_method.PATERN_NUM;
+        String patternNum = inputMethodNum.getInput();
+        Pattern rNum = Pattern.compile(patternNum);
+        Matcher mNum = rNum.matcher(data[0]);
+        Boolean numBoolean = mNum.matches();
+        if (numBoolean == true) {
+            System.out.println("ЧИСЛО");
+
+        }
 
         Input_method inputMethodArab = Input_method.PATERN_ARAB;
-        Input_method inputMethodRom = Input_method.PATERN_ROM;
         String patternArab = inputMethodArab.getInput();
-        String patternRom = inputMethodRom.getInput();
-        Pattern r;
-        Matcher m;
+        Pattern rArab;
+        rArab = Pattern.compile(patternArab);
+        Matcher mArab = rArab.matcher(inputString);
+        Boolean arabBoolean = mArab.matches();
+        if (arabBoolean == true) {
 
-        if (Character.isDigit(firstCharacter) == false) {
-            r = Pattern.compile(patternRom);
-            m = r.matcher(inputString);
-            operatorSelection = m.matches();
-            return;
-        } else {
-            r = Pattern.compile(patternArab);
-            m = r.matcher(inputString);
-            operatorSelection = m.matches();
-
+            System.out.println("ARAB ЧИСЛО");
         }
 
-        System.out.println("ARAB " + operatorsStrings[0]);
+        Input_method inputMethodRom = Input_method.PATERN_ROM;
+        String patternRom = inputMethodRom.getInput();
+        Pattern rRom = Pattern.compile(patternRom);
+        Matcher mRom = rRom.matcher(inputString);
+        Boolean romBoolean = mRom.matches();
+        if (romBoolean == arabBoolean) {
 
+            System.out.println("ROM ЧИСЛО");
+        }
+
+        if (numBoolean == true & arabBoolean == true) {
+
+            switch (operator) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    System.out.println("Output:");
+                    System.out.println(result);
+                    break;
+
+                case "-":
+                    result = firstNumber - secondNumber;
+                    System.out.println("Output:");
+                    System.out.println(result);
+                    break;
+
+                case "*":
+                    result = firstNumber * secondNumber;
+                    System.out.println("Output:");
+                    System.out.println(result);
+                    break;
+
+                case "/":
+                    result = firstNumber / secondNumber;
+                    System.out.println("Output:");
+                    System.out.println(result);
+                    break;
+            }
+
+            System.out.println("Один формат");
+        } else {
+            inputErrors = Input_errors.DIFFERENT_NUMBER_SYSTEMS;
+            inputErrorsString = inputErrors.getError();
+            System.out.println(inputErrorsString);
+            return;
+        }
     }
 
 }
--- src/Roman_numerals.java
+++ src/Roman_numerals.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public enum Roman_numerals {
+public enum Roman_numerals {
+    I("1"),
+    III("3"),
+    IV("4"),
+    V("5"),
+    IX("9"),
+    X("10");
+    
+    String displayRomNum;
+    String arabNumString = "I";
+
+    Roman_numerals(String displayRomNum) {
+        this.displayRomNum = displayRomNum;
+    }
+
+    String setRomNum(String arabNumString) {
+
+        switch (arabNumString) {
+            case "I":
+                displayRomNum = "1";
+        }
+        return displayRomNum;
+    }
+
+    String getRomNum() {
+        return displayRomNum;
+    }
 }

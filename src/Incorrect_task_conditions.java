4b4b8b66bd0335877a2ac857c488ad7eff1a281a
Moriyck <Sim19720504@yandex.ru>
2023-01-17 15:44:01 +0300

    З2

--- src/Incorrect_task_conditions.java
+++ src/Incorrect_task_conditions.java
@@ -1,5 +1,9 @@
 public class Incorrect_task_conditions extends  Exception{
     Incorrect_task_conditions(String description){
+
+
+
+        
 super(description);
     }
 }
--- src/Input_method.java
+++ src/Input_method.java
@@ -1,4 +1,5 @@
 public enum Input_method {
+    PATERN_NUM("^[1-9][0]*$"),
     PATERN_ARAB("^[1-9][0]*[\\+\\/\\*\\-]*[1-9][0]*$"),
     PATERN_ROM("^[IiVvXx]*[\\+\\/\\*\\-]*[IiVvXx]*$");
 
